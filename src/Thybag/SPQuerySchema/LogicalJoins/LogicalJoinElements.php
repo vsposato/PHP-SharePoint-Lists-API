498bdc75a8e6e6844684ea721966a931d7e62ff0
Vincent Sposato <vsposato@ufl.edu>
2014-05-10 05:38:33 -0400

    Begin work on the LogicalJoin operator class and tests

--- src/Thybag/SPQuerySchema/LogicalJoins/LogicalJoinElements.php
+++ src/Thybag/SPQuerySchema/LogicalJoins/LogicalJoinElements.php
@@ -13,22 +13,20 @@
 
     class LogicalJoinElements extends SPQuerySchema {
 
-        public static function ArrayJoinWithAnd( $fieldReferencesArray = array() ) {
+        public static function ArrayJoinWithAnd( $comparisonOperatorDefinitions = array() ) {
+
+            if ( ! isset($comparisonOperatorDefinitions['And']) || count($comparisonOperatorDefinitions) != 1 ) {
+                throw new \InvalidArgumentException("Must be a single And array provided to this function!");
+            }
 
             // Check to see how many conditions there are, as Logical Joins can only have 2 items in them
-            $countOfTopLevelItems = count($fieldReferencesArray);
+            $countOfTopLevelItems = count($comparisonOperatorDefinitions);
 
             $numberOfAndsNeeded = (int) ceil($countOfTopLevelItems / 2);
 
             $countOfLoops = 1;
             // Loop through each parent element and start building array
-            foreach ($fieldReferencesArray as $comparisonKey => $definitionArray ) {
-                // Handle Field References
-                if (array_key_exists('FieldRef', $definitionArray)) {
-                    // Get a Field Ref XML back from the Field Ref XML Class
-
-                }
-
+            foreach ($comparisonOperatorDefinitions as $comparisonOperatorKey => $comparisonOperatorValue ) {
 
             }
         }
--- test/Thybag/SPQuerySchema/LogicalJoins/LogicalJoinElementsTest.php
+++ test/Thybag/SPQuerySchema/LogicalJoins/LogicalJoinElementsTest.php
@@ -44,32 +44,309 @@ class LogicalJoinElementsTest extends \PHPUnit_Framework_TestCase {
 
         $resultCAML = LogicalJoinElements::ArrayJoinWithAnd($inputCAMLArray);
 
-        $this->assertXmlStringEqualsXmlString($expectedReturn, $resultCAML);
+        $this->assertEquals($expectedReturn, $resultCAML);
     }
 
     public function arrayAndProvider() {
-        $expectedReturn1 = <<<XML
+        $expectedReturn1 = <<<XML1
 <And>
     <BeginsWith>
-        <FieldRef name="LastName" />
+        <FieldRef name="LastName"/>
         <Value type="Integer">1</Value>
     </BeginsWith>
 </And>
-XML;
+XML1;
+
+        $beginsWithXMLStringXMLDouble = <<<XML1
+<And>
+    <BeginsWith>
+        <FieldRef Name="_LastName"/>
+        <Value Type="Text">Smith</Value>
+    </BeginsWith>
+    <BeginsWith>
+        <FieldRef Name="_FirstName"/>
+        <Value Type="Text">John</Value>
+    </BeginsWith>
+</And>
+XML1;
+        $beginsWithXMLStringXMLTriple = <<<XML2
+<And>
+    <And>
+        <BeginsWith>
+            <FieldRef Name="_LastName"/>
+            <Value Type="Text">Smith</Value>
+        </BeginsWith>
+        <BeginsWith>
+            <FieldRef Name="_FirstName"/>
+            <Value Type="Text">John</Value>
+        </BeginsWith>
+    </And>
+    <And>
+        <BeginsWith>
+            <FieldRef Name="_MiddleName"/>
+            <Value Type="Text">Q</Value>
+        </BeginsWith>
+    </And>
+</And>
+XML2;
+        $beginsWithXMLStringXMLQuadruple = <<<XML2
+<And>
+    <And>
+        <BeginsWith>
+            <FieldRef Name="_LastName"/>
+            <Value Type="Text">Smith</Value>
+        </BeginsWith>
+        <BeginsWith>
+            <FieldRef Name="_FirstName"/>
+            <Value Type="Text">John</Value>
+        </BeginsWith>
+    </And>
+    <And>
+        <BeginsWith>
+            <FieldRef Name="_MiddleName"/>
+            <Value Type="Text">Q</Value>
+        </BeginsWith>
+        <BeginsWith>
+            <FieldRef Name="City"/>
+            <Value Type="Text">Gainesville</Value>
+        </BeginsWith>
+    </And>
+</And>
+XML2;
+        $beginsWithXMLStringXMLQuintuple = <<<XML2
+<And>
+    <And>
+        <And>
+            <BeginsWith>
+                <FieldRef Name="_LastName"/>
+                <Value Type="Text">Smith</Value>
+            </BeginsWith>
+            <BeginsWith>
+                <FieldRef Name="_FirstName"/>
+                <Value Type="Text">John</Value>
+            </BeginsWith>
+        </And>
+        <And>
+            <BeginsWith>
+                <FieldRef Name="_MiddleName"/>
+                <Value Type="Text">Q</Value>
+            </BeginsWith>
+            <BeginsWith>
+                <FieldRef Name="City"/>
+                <Value Type="Text">Gainesville</Value>
+            </BeginsWith>
+        </And>
+    </And>
+    <And>
+        <BeginsWith>
+            <FieldRef Name="State"/>
+            <Value Type="Text">Florida</Value>
+        </BeginsWith>
+    </And>
+</And>
+XML2;
+
 
         return array(
             array(
                 array(
-                    'BeginsWith' => array(
-                        'FieldRef' => array(
-                            'name' => 'LastName'
-                        ),
-                        'Value' => array(
-                            'type' => 'Integer',
-                            'value' => 1
+                    'And' => array(
+                        array(
+                            'BeginsWith' => array(
+                                'FieldRef' => array(
+                                    'name' => 'LastName'
+                                ),
+                                'Value'    => array(
+                                    'type'  => 'Integer',
+                                    'value' => 1
+                                )
+                            )
                         )
-                    )),
+                    )
+                ),
                 $expectedReturn1
+            ),
+            array(
+                array(
+                    'And' => array(
+                        array(
+                            'BeginsWith' => array(
+                                'FieldRef' => array(
+                                    'Name' => '_LastName'
+                                ),
+                                'Value'    => array(
+                                    'Type'  => 'Text',
+                                    'Value' => 'Smith'
+                                )
+                            )
+                        ),
+                        array(
+                            'BeginsWith' => array(
+                                'FieldRef' => array(
+                                    'Name' => '_LastName'
+                                ),
+                                'Value'    => array(
+                                    'Type'  => 'Text',
+                                    'Value' => 'Smith'
+                                )
+                            )
+                        )
+                    )
+                ),
+                $beginsWithXMLStringXMLDouble
+            ),
+            array(
+                array(
+                    'And' => array(
+                        array(
+                            'BeginsWith' => array(
+                                'FieldRef' => array(
+                                    'Name' => '_LastName'
+                                ),
+                                'Value'    => array(
+                                    'Type'  => 'Text',
+                                    'Value' => 'Smith'
+                                )
+                            )
+                        ),
+                        array(
+                            'BeginsWith' => array(
+                                'FieldRef' => array(
+                                    'Name' => '_LastName'
+                                ),
+                                'Value'    => array(
+                                    'Type'  => 'Text',
+                                    'Value' => 'Smith'
+                                )
+                            )
+                        ),
+                        array(
+                            'BeginsWith' => array(
+                                'FieldRef' => array(
+                                    'Name' => '_LastName'
+                                ),
+                                'Value'    => array(
+                                    'Type'  => 'Text',
+                                    'Value' => 'Smith'
+                                )
+                            )
+                        )
+                    )
+                ),
+                $beginsWithXMLStringXMLTriple
+            ),
+            array(
+                array(
+                    'And' => array(
+                        array(
+                            'BeginsWith' => array(
+                                'FieldRef' => array(
+                                    'Name' => '_LastName'
+                                ),
+                                'Value'    => array(
+                                    'Type'  => 'Text',
+                                    'Value' => 'Smith'
+                                )
+                            )
+                        ),
+                        array(
+                            'BeginsWith' => array(
+                                'FieldRef' => array(
+                                    'Name' => '_LastName'
+                                ),
+                                'Value'    => array(
+                                    'Type'  => 'Text',
+                                    'Value' => 'Smith'
+                                )
+                            )
+                        ),
+                        array(
+                            'BeginsWith' => array(
+                                'FieldRef' => array(
+                                    'Name' => '_LastName'
+                                ),
+                                'Value'    => array(
+                                    'Type'  => 'Text',
+                                    'Value' => 'Smith'
+                                )
+                            )
+                        ),
+                        array(
+                            'BeginsWith' => array(
+                                'FieldRef' => array(
+                                    'Name' => 'City'
+                                ),
+                                'Value'    => array(
+                                    'Type'  => 'Text',
+                                    'Value' => 'Gainesville'
+                                )
+                            )
+                        )
+                    )
+                ),
+                $beginsWithXMLStringXMLQuadruple
+            ),
+            array(
+                array(
+                    'And' => array(
+                        array(
+                            'BeginsWith' => array(
+                                'FieldRef' => array(
+                                    'Name' => '_LastName'
+                                ),
+                                'Value'    => array(
+                                    'Type'  => 'Text',
+                                    'Value' => 'Smith'
+                                )
+                            )
+                        ),
+                        array(
+                            'BeginsWith' => array(
+                                'FieldRef' => array(
+                                    'Name' => '_LastName'
+                                ),
+                                'Value'    => array(
+                                    'Type'  => 'Text',
+                                    'Value' => 'Smith'
+                                )
+                            )
+                        ),
+                        array(
+                            'BeginsWith' => array(
+                                'FieldRef' => array(
+                                    'Name' => '_LastName'
+                                ),
+                                'Value'    => array(
+                                    'Type'  => 'Text',
+                                    'Value' => 'Smith'
+                                )
+                            )
+                        ),
+                        array(
+                            'BeginsWith' => array(
+                                'FieldRef' => array(
+                                    'Name' => 'City'
+                                ),
+                                'Value'    => array(
+                                    'Type'  => 'Text',
+                                    'Value' => 'Gainesville'
+                                )
+                            )
+                        ),
+                        array(
+                            'BeginsWith' => array(
+                                'FieldRef' => array(
+                                    'Name' => 'State'
+                                ),
+                                'Value'    => array(
+                                    'Type'  => 'Text',
+                                    'Value' => 'Florida'
+                                )
+                            )
+                        )
+                    )
+                ),
+                $beginsWithXMLStringXMLQuintuple
             )
         );
     }
