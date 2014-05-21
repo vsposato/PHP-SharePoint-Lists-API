073ca3a18d91a1c39a63754e815dc024c868d77b
Vincent Sposato <vincent.sposato@gmail.com>
2014-05-20 20:25:19 -0400

    Working on XMLHandler test

--- test/Thybag/XML/XMLHandlerTest.php
+++ test/Thybag/XML/XMLHandlerTest.php
@@ -45,12 +45,11 @@
          * @author  Vincent Sposato <vsposato@ufl.edu>
          * @version 1.0
          */
-        public function testConvertXMLToArray( $inputXML, $expectedArrayResults ) {
+        public function testConvertXMLToArray( $expectedArrayResults, $inputXML  ) {
 
             $xmlToArrayObject = new XMLHandler();
 
             $results = $xmlToArrayObject->convertXmlToArray($inputXML);
-
             foreach ($results as $key => $result) {
                 $this->assertArrayHasKey($key, $expectedArrayResults);
             }
@@ -58,10 +57,174 @@
         }
 
         public function XmlToArrayProvider() {
+            $beginsWithXMLStringXML = <<<XML1
+<Root><BeginsWith><FieldRef Name="_LastName" /><Value Type="Text">Smith</Value></BeginsWith></Root>
+XML1;
+            $beginsWithXMLStringXMLDouble = <<<XML1
+<Root><BeginsWith><FieldRef Name="_LastName" /><Value Type="Text">Smith</Value></BeginsWith><BeginsWith><FieldRef Name="_LastName" /><Value Type="Text">Smith</Value></BeginsWith></Root>
+XML1;
+            $dateRangesOverlapStringXML = <<<XML2
+<Root><DateRangesOverlap><FieldRef Name="PromDate" /><Value Type="DateTime">2014-04-01</Value></DateRangesOverlap></Root>
+XML2;
+            $dateRangesOverlapStringXMLIncludeTime = <<<XML3
+<Root><DateRangesOverlap><FieldRef Name="PromDate" /><Value Type="DateTime" IncludeTimeValue="True">2014-04-01 08:00</Value></DateRangesOverlap></Root>
+XML3;
+            $inStringXML = <<<XML4
+<Root><In><FieldRef Name="_LastName" /><Values><Value Type="Text">Smith</Value><Value Type="Text">Pence</Value></Values></In></Root>
+XML4;
 
+            $inStringXMLWithXMLIsland = <<<XML5
+<Root><In><FieldRef Name="_LastName" /><Values><Value Type="Text">Smith</Value><Value Type="Text">Pence</Value></Values><XML><SetVar Name="GlobalVar" Scope="Request">Bar</SetVar></XML></In></Root>
+XML5;
             return array(
                 array(
-
+                    array(
+                        'BeginsWith' => array(
+                            'FieldRef' => array(
+                                '@attributes' => array(
+                                    'Name' => '_LastName'
+                                )
+                            ),
+                            'Value'    => array(
+                                '@attributes' => array(
+                                    'Type'  => 'Text',
+                                ),
+                                'Value' => 'Smith'
+                            )
+                        )
+                    ),
+                    $beginsWithXMLStringXML
+                ),
+                array(
+                    array(
+                        array(
+                            'BeginsWith' => array(
+                                'FieldRef' => array(
+                                    '@attributes' => array(
+                                        'Name' => '_LastName'
+                                    )
+                                ),
+                                'Value'    => array(
+                                    '@attributes' => array(
+                                        'Type'  => 'Text'
+                                    ),
+                                    'Value' => 'Smith'
+                                )
+                            )
+                        ),
+                        array(
+                            'BeginsWith' => array(
+                                'FieldRef' => array(
+                                    '@attributes' => array(
+                                        'Name' => '_LastName'
+                                    )
+                                ),
+                                'Value'    => array(
+                                    '@attributes' => array(
+                                        'Type'  => 'Text',
+                                    ),
+                                    'Value' => 'Smith'
+                                )
+                            )
+                        )
+                    ),
+                    $beginsWithXMLStringXMLDouble
+                ),
+                array(
+                    array(
+                        'DateRangesOverlap' => array(
+                            'FieldRef' => array(
+                                '@attributes' => array(
+                                    'Name' => 'PromDate'
+                                )
+                            ),
+                            'Value'    => array(
+                                '@attributes' => array(
+                                    'Type'  => 'DateTime',
+                                ),
+                                'Value' => '2014-04-01'
+                            )
+                        )
+                    ),
+                    $dateRangesOverlapStringXML
+                ),
+                array(
+                    array(
+                        'DateRangesOverlap' => array(
+                            'FieldRef' => array(
+                                '@attributes' => array(
+                                    'Name' => 'PromDate'
+                                )
+                            ),
+                            'Value'    => array(
+                                '@attributes' => array(
+                                    'Type'             => 'DateTime',
+                                    'IncludeTimeValue' => 'True'
+                                ),
+                                'Value'            => '2014-04-01 08:00'
+                            )
+                        )
+                    ),
+                    $dateRangesOverlapStringXMLIncludeTime
+                ),
+                array(
+                    array(
+                        'In' => array(
+                            'FieldRef' => array(
+                                '@attributes' => array(
+                                    'Name' => '_LastName'
+                                )
+                            ),
+                            'Values'   => array(
+                                array(
+                                    'Value' => array(
+                                        '@attributes' => array(
+                                            'Type'  => 'Text'
+                                        ),
+                                        'Value' => 'Smith'
+                                    )
+                                ),
+                                array(
+                                    'Value' => array(
+                                        '@attributes' => array(
+                                            'Type'  => 'Text'
+                                        ),
+                                        'Value' => 'Pence',
+                                    )
+                                )
+                            )
+                        )
+                    ),
+                    $inStringXML
+                ),
+                array(
+                    array(
+                        'In' => array(
+                            'FieldRef' => array(
+                                '@attributes' => array(
+                                    'Name' => '_LastName'
+                                )
+                            ),
+                            'Values'   => array(
+                                array(
+                                    '@attributes' => array(
+                                        'Type'  => 'Text',
+                                    ),
+                                    'Value' => 'Smith'
+                                ),
+                                array(
+                                    '@attributes' => array(
+                                        'Type'  => 'Text',
+                                    ),
+                                    'Value' => 'Pence',
+                                )
+                            ),
+                            'XML'      => array(
+                                '<SetVar Name="GlobalVar" Scope="Request">Bar</SetVar>'
+                            )
+                        )
+                    ),
+                    $inStringXMLWithXMLIsland
                 )
             );
         }
